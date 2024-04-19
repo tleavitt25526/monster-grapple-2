efc4251b365db4cee26982ca3090c327cf693250
tleavitt25526 <98976857+tleavitt25526@users.noreply.github.com>
2024-04-19 15:31:47 -0400

    LET ME PUSH!!!

--- main.ts
+++ main.ts
@@ -3,26 +3,71 @@ namespace SpriteKind {
     export const Particle = SpriteKind.create()
     export const Cursor = SpriteKind.create()
 }
-/**
- * Maybe make a different variable for embers being active versus the menu.
- */
+
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (menu_active) {
         if (declutter.get("cursor").y == 85) {
             declutter.get("cursor").y += -10
         }
+    } else if (options_active) {
+        difficulty += 1
+        difficulty = Math.constrain(difficulty, 0, 3)
+        declutter.get("diff").setImage(difficulty_icons[difficulty])
+    } else if (levels_active) {
+
     }
 })
+function LoadSelect() {
+    levels_active = true
+    declutter.load("one", fancyText.create("Lava Pits", 120, 0, fancyText.defaultArcade))
+    declutter.get("one").y += -20
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (menu_active) {
         if (declutter.get("cursor").y == 75) {
-            unloadMenu()
+            UnloadMenu()
+            LoadSelect()
+        } else if (declutter.get("cursor").y == 85) {
+            UnloadMenu()
+            LoadOptions()
         }
+    } else if (options_active) {
+        UnloadOptions()
+        LoadMenu()
     }
 })
-function loadMenu () {
+function UnloadOptions() {
+    options_active = false
+    declutter.offload("diff")
+}
+function UnloadLevel() {
+    levels_active = false
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (menu_active) {
+        if (declutter.get("cursor").y == 75) {
+            declutter.get("cursor").y += 10
+        }
+    } else if (options_active) {
+        difficulty += -1
+        difficulty = Math.constrain(difficulty, 0, 3)
+        declutter.get("diff").setImage(difficulty_icons[difficulty])
+    } else if (levels_active) {
+
+    }
+})
+function UnloadMenu() {
+    menu_active = false
+    declutter.offload("line")
+    declutter.offload("cursor")
+    declutter.offload("options")
+    declutter.offload("text")
+    declutter.offload("start")
+}
+function LoadMenu() {
     declutter.load("text", fancyText.create("Monster Grapple", 120, 0, fancyText.serif_small))
     fancyText.animateAtSpeed(declutter.get("text"), fancyText.TextSpeed.Normal, fancyText.AnimationPlayMode.InBackground)
+    ember_active = true
     timer.after(1500, function () {
         declutter.load("line", sprites.create(img`
             b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b 
@@ -33,7 +78,7 @@ function loadMenu () {
     timer.after(2000, function () {
         declutter.load("start", fancyText.create("start", 120, 0, fancyText.defaultArcade))
         declutter.get("start").y += 15
-        declutter.load("cursor", sprites.create(assets.image`select`, SpriteKind.Cursor))
+        declutter.load("cursor", sprites.create(assets.image`select`, SpriteKind.GUI))
         declutter.get("cursor").setPosition(15, 75)
     })
     timer.after(2100, function () {
@@ -42,20 +87,29 @@ function loadMenu () {
         declutter.get("options").y += 25
     })
 }
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (menu_active) {
-        if (declutter.get("cursor").y == 75) {
-            declutter.get("cursor").y += 10
-        }
-    }
-})
-function unloadMenu () {
-	
+
+function LoadOptions() {
+    options_active = true
+    declutter.load("diff", sprites.create(difficulty_icons[difficulty], SpriteKind.GUI))
 }
+let ember_active = false
+let levels_active = false
+let options_active = false
 let menu_active = false
-loadMenu()
+let difficulty = 0
+let level = 0
+let difficulty_icons: Image[] = []
+//stats.turnStats(true)
+LoadMenu()
+difficulty_icons = [
+    assets.image`easy_icon`,
+    assets.image`normal_icon`,
+    assets.image`hard_icon`,
+    assets.image`impossible_icon`
+]
+
 game.onUpdateInterval(100, function () {
-    if (menu_active) {
+    if (ember_active) {
         if (Math.percentChance(50)) {
             declutter.load("ember", sprites.create(assets.image`ember`, SpriteKind.Particle))
         } else {
